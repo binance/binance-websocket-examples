8d11cd38b8e701160015080a2c2856abf5e9a2f0
Liang Shi <liang.shi@protonmail.com>
2021-01-13 07:50:34 +1100

    clean up

--- src/spot/monitor-depth.js
+++ src/spot/monitor-depth.js
@@ -8,8 +8,6 @@ const streamName = 'btcusdt@depth';
 const socketClient = new SocketClient(`ws/${streamName}`, 'wss://stream.binance.com:9443/');
 socketClient.setHandler('depthUpdate', (params) => {
   const current = +new Date;
-  // const TvsNow = current - params.T;
   const EvsNow = current - params.E;
-  // const EvsT = params.E - params.T;
   console.log(`[Spot ${streamName}] current:${current} delta EvsNow: ${EvsNow}`);
 });
