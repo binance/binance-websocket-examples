89c976c7218d9eea9f30456c29703f83749f9948
Liang Shi <liang.shi@protonmail.com>
2021-01-13 07:59:20 +1100

    add time gap from last event

--- src/spot/monitor-depth.js
+++ src/spot/monitor-depth.js
@@ -3,11 +3,14 @@
 import SocketClient from '../lib/socketClient';
 
 
-const streamName = 'btcusdt@depth';
+const streamName = 'btcusdt@depth@100ms';
 
 const socketClient = new SocketClient(`ws/${streamName}`, 'wss://stream.binance.com:9443/');
+let lastCurrent = +new Date;
 socketClient.setHandler('depthUpdate', (params) => {
   const current = +new Date;
   const EvsNow = current - params.E;
-  console.log(`[Spot ${streamName}] current:${current} delta EvsNow: ${EvsNow}`);
+  const fromLastEvent = current - lastCurrent
+  console.log(`[Spot ${streamName}] current:${current} fromLastEvent: ${fromLastEvent} delta EvsNow: ${EvsNow}`);
+  lastCurrent = current;
 });
